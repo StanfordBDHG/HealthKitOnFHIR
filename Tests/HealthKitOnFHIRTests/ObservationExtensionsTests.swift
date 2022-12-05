c3bc36e71645da34a8170bf8ddfdaeb2c8e2a373
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-12-05 09:52:27 -0800

    Improve Tests

--- Tests/HealthKitOnFHIRTests/ObservationExtensionsTests.swift
+++ Tests/HealthKitOnFHIRTests/ObservationExtensionsTests.swift
@@ -11,14 +11,48 @@ import XCTest
 
 
 final class ObservationExtensionsTests: XCTestCase {
-    func testCollectionExtensions() throws {
+    func testCollectionExtensionsIdentifier() throws {
         // swiftlint:disable:previous function_body_length
         // We disable the function body length as this is a test case
         let observation = Observation(code: CodeableConcept(), status: FHIRPrimitive(.final))
         
+        // First test all extensions with no value beeing present (collection is nil)
         observation.appendIdentifier(
             Identifier(id: "ID1")
         )
+        
+        // Assertions for the nil/non-present case:
+        XCTAssertEqual(
+            observation.identifier?.first,
+            Identifier(id: "ID1")
+        )
+        
+        
+        // Now Append multiple elements and in a non-nil collection:
+        observation.appendIdentifiers(
+            [
+                Identifier(id: "ID2"),
+                Identifier(id: "ID3")
+            ]
+        )
+        
+        // Assertions for the non-nil collection case:
+        XCTAssertEqual(
+            observation.identifier,
+            [
+                Identifier(id: "ID1"),
+                Identifier(id: "ID2"),
+                Identifier(id: "ID3")
+            ]
+        )
+    }
+    
+    func testCollectionExtensionsCoding() throws {
+        // swiftlint:disable:previous function_body_length
+        // We disable the function body length as this is a test case
+        let observation = Observation(code: CodeableConcept(), status: FHIRPrimitive(.final))
+        
+        // First test all extensions with no value beeing present (collection is nil)
         observation.appendCoding(
             Coding(
                 code: "Code1",
@@ -26,14 +60,8 @@ final class ObservationExtensionsTests: XCTestCase {
                 system: FHIRPrimitive(FHIRURI(stringLiteral: "https://test1.system"))
             )
         )
-        observation.appendCategory(
-            CodeableConcept(id: "Concept1")
-        )
         
-        XCTAssertEqual(
-            observation.identifier?.first,
-            Identifier(id: "ID1")
-        )
+        // Assertions for the nil/non-present case:
         XCTAssertEqual(
             observation.code.coding?.first,
             Coding(
@@ -42,19 +70,9 @@ final class ObservationExtensionsTests: XCTestCase {
                 system: FHIRPrimitive(FHIRURI(stringLiteral: "https://test1.system"))
             )
         )
-        XCTAssertEqual(
-            observation.category?.first,
-            CodeableConcept(id: "Concept1")
-        )
         
-        // Append multiple elements and in a non-nil collection:
         
-        observation.appendIdentifiers(
-            [
-                Identifier(id: "ID2"),
-                Identifier(id: "ID3")
-            ]
-        )
+        // Now Append multiple elements and in a non-nil collection:
         observation.appendCodings(
             [
                 Coding(
@@ -69,37 +87,8 @@ final class ObservationExtensionsTests: XCTestCase {
                 )
             ]
         )
-        observation.appendCategories(
-            [
-                CodeableConcept(id: "Concept2"),
-                CodeableConcept(id: "Concept3")
-            ]
-        )
-        observation.appendComponent(
-            ObservationComponent(
-                code: CodeableConcept(id: "Concept4"),
-                value: .boolean(true.asPrimitive())
-            )
-        )
-        observation.appendComponents([
-            ObservationComponent(
-                code: CodeableConcept(id: "Concept5"),
-                value: .string("Test".asFHIRStringPrimitive())
-            ),
-            ObservationComponent(
-                code: CodeableConcept(id: "Concept6"),
-                value: .integer(10.asFHIRIntegerPrimitive())
-            )
-        ])
         
-        XCTAssertEqual(
-            observation.identifier,
-            [
-                Identifier(id: "ID1"),
-                Identifier(id: "ID2"),
-                Identifier(id: "ID3")
-            ]
-        )
+        // Assertions for the non-nil collection case:
         XCTAssertEqual(
             observation.code.coding,
             [
@@ -120,6 +109,33 @@ final class ObservationExtensionsTests: XCTestCase {
                 )
             ]
         )
+    }
+    
+    func testCollectionExtensionsCategories() throws {
+        // swiftlint:disable:previous function_body_length
+        // We disable the function body length as this is a test case
+        let observation = Observation(code: CodeableConcept(), status: FHIRPrimitive(.final))
+        
+        // First test all extensions with no value beeing present (collection is nil)
+        observation.appendCategory(
+            CodeableConcept(id: "Concept1")
+        )
+        
+        // Assertions for the nil/non-present case:
+        XCTAssertEqual(
+            observation.category?.first,
+            CodeableConcept(id: "Concept1")
+        )
+        
+        // Now Append multiple elements and in a non-nil collection:
+        observation.appendCategories(
+            [
+                CodeableConcept(id: "Concept2"),
+                CodeableConcept(id: "Concept3")
+            ]
+        )
+
+        // Assertions for the non-nil collection case:
         XCTAssertEqual(
             observation.category,
             [
@@ -128,17 +144,60 @@ final class ObservationExtensionsTests: XCTestCase {
                 CodeableConcept(id: "Concept3")
             ]
         )
-        XCTAssertEqual(
-            observation.component?[0].value,
-            .boolean(true.asPrimitive())
+    }
+    
+    func testCollectionExtensionsComponents() throws {
+        // swiftlint:disable:previous function_body_length
+        // We disable the function body length as this is a test case
+        let observation = Observation(code: CodeableConcept(), status: FHIRPrimitive(.final))
+        
+        // First test all extensions with no value beeing present (collection is nil)
+        observation.appendComponent(
+            ObservationComponent(
+                code: CodeableConcept(id: "Concept4"),
+                value: .boolean(true.asPrimitive())
+            )
         )
+        
+        // Assertions for the nil/non-present case:
         XCTAssertEqual(
-            observation.component?[1].value,
-            .string("Test".asFHIRStringPrimitive())
+            observation.component?.first,
+            ObservationComponent(
+                code: CodeableConcept(id: "Concept4"),
+                value: .boolean(true.asPrimitive())
+            )
         )
+        
+        
+        // Now Append multiple elements and in a non-nil collection:
+        observation.appendComponents([
+            ObservationComponent(
+                code: CodeableConcept(id: "Concept5"),
+                value: .string("Test".asFHIRStringPrimitive())
+            ),
+            ObservationComponent(
+                code: CodeableConcept(id: "Concept6"),
+                value: .integer(10.asFHIRIntegerPrimitive())
+            )
+        ])
+        
+        // Assertions for the non-nil collection case:
         XCTAssertEqual(
-            observation.component?[2].value,
-            .integer(10.asFHIRIntegerPrimitive())
+            observation.component,
+            [
+                ObservationComponent(
+                    code: CodeableConcept(id: "Concept4"),
+                    value: .boolean(true.asPrimitive())
+                ),
+                ObservationComponent(
+                    code: CodeableConcept(id: "Concept5"),
+                    value: .string("Test".asFHIRStringPrimitive())
+                ),
+                ObservationComponent(
+                    code: CodeableConcept(id: "Concept6"),
+                    value: .integer(10.asFHIRIntegerPrimitive())
+                )
+            ]
         )
     }
 }
