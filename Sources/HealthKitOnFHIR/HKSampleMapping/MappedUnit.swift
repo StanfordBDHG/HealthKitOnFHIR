974821e76972b9ad6e29924db6073a1b3dddbb0a
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-12-09 15:40:23 -0500

    Map units to standardized UCUM codes (#19)

--- Sources/HealthKitOnFHIR/HKSampleMapping/MappedUnit.swift
+++ Sources/HealthKitOnFHIR/HKSampleMapping/MappedUnit.swift
@@ -9,29 +9,43 @@
 public struct MappedUnit: Codable {
     private enum CodingKeys: String, CodingKey {
         case hkunit
-        case unitAlias
+        case unit
+        case system
+        case code
     }
 
     public let hkunit: String
-    public let unitAlias: String?
-
+    public let unit: String
+    public let system: URL?
+    public let code: String?
 
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         let hkunit = try values.decode(String.self, forKey: .hkunit)
-        let unitAlias = try values.decodeIfPresent(String.self, forKey: .unitAlias)
+        let unit = try values.decode(String.self, forKey: .unit)
+        let system = try values.decodeIfPresent(URL.self, forKey: .system)
+        let code = try values.decodeIfPresent(String.self, forKey: .code)
 
-        self.init(hkunit: hkunit, unitAlias: unitAlias)
+        self.init(
+            hkunit: hkunit,
+            unit: unit,
+            system: system,
+            code: code
+        )
     }
 
     public init(
         hkunit: String,
-        unitAlias: String?
+        unit: String,
+        system: URL?,
+        code: String?
     ) {
         // Unfortunately this method throws an Objective-C exception when an error occurs, we can not catch this here.
         _ = HKUnit(from: hkunit)
 
         self.hkunit = hkunit
-        self.unitAlias = unitAlias
+        self.unit = unit
+        self.system = system
+        self.code = code
     }
 }
