ae974ece216093d16a4c50c2a6e79741f09f852e
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-12-15 08:05:43 -0500

    Link units and healthkit types

--- support_table_generator.py
+++ support_table_generator.py
@@ -117,32 +117,41 @@ markdown_file.write(reuse_header + '\n\n' + '# HKObject Support Table ' + '\n\n'
 quantity_types = data['HKQuantitySamples']
 rows = []
 
-for type in quantity_types:
-    formatted_type = type.removeprefix('HKQuantityTypeIdentifier')
-    
-    code = quantity_types[type]['codings'][0]['code']
+for type in quantity_types:    
+    # Format code links
+    coding = quantity_types[type]['codings'][0]
+    code = coding['code']
     code_system = quantity_types[type]['codings'][0]['system']
 
-    if code_system == "http://loinc.org":
+    if code_system == 'http://loinc.org' or code_system == 'https://developer.apple.com/documentation/healthkit':
         code_url = '{}/{}'.format(code_system, code)
     else:
         code_url = code_system
 
     linked_code = '[{}]({})'.format(code, code_url)
 
-    unit = quantity_types[type]['unit']['unit']
+    # Format unit links
+    unit_coding = quantity_types[type]['unit']
+    unit = unit_coding['unit']
+    if 'system' in unit_coding: # Some units are not from a code system
+        linked_unit = '[{}]({})'.format(unit, unit_coding['system'])
+    else:
+        linked_unit = unit
     
-    row = [formatted_type, ':white_check_mark:', linked_code, unit]
+    row = [type, ':white_check_mark:', linked_code, linked_unit]
     rows.append(row)
 
 # Add all the unsupported types
 for type in all_quantity_types:
     if type not in quantity_types:
-        formatted_type = type.removeprefix('HKQuantityTypeIdentifier')
-        rows.append([formatted_type, ":x:", "-", "-"])
+        rows.append([type, ":x:", "-", "-"])
 
 # Sort the rows alphabetically
 rows = sorted(rows, key=itemgetter(0))
+
+# Link all the HealthKit types to Apple docs
+for type in rows:
+    type[0] = '[{}]({})'.format(type[0].removeprefix('HKQuantityTypeIdentifier'), 'https://developer.apple.com/documentation/healthkit/' + type[0])
 
 # Write the statistics
 stats = 'HealthKitOnFHIR supports {} of {} quantity types.'.format(len(quantity_types), len(all_quantity_types))
