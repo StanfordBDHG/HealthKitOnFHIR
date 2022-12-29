401301411b81fef7ebe332b079e49601caa9d02a
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-12-29 14:52:29 -0500

    Add additional type conversions

--- Sources/HealthKitOnFHIR/HealthKit Extensions/HKClinicalRecord+Resource.swift
+++ Sources/HealthKitOnFHIR/HealthKit Extensions/HKClinicalRecord+Resource.swift
@@ -13,13 +13,52 @@ import ModelsR4
 extension HKClinicalRecord {
     public var allergyIntolerance: AllergyIntolerance {
         get throws {
-            guard let resource = try buildResource() as? AllergyIntolerance else {
-                throw HealthKitOnFHIRError.invalidFHIRResource
-            }
-            return resource
+            return try convert(AllergyIntolerance.self)
         }
     }
-    
+
+    public var medicationDispense: MedicationDispense {
+        get throws {
+            return try convert(MedicationDispense.self)
+        }
+    }
+
+    public var medicationRequest: MedicationRequest {
+        get throws {
+            return try convert(MedicationRequest.self)
+        }
+    }
+
+    public var medicationStatement: MedicationStatement {
+        get throws {
+            return try convert(MedicationStatement.self)
+        }
+    }
+
+    public var condition: Condition {
+        get throws {
+            return try convert(Condition.self)
+        }
+    }
+
+    public var coverage: Coverage {
+        get throws {
+            return try convert(Coverage.self)
+        }
+    }
+
+    public var immunization: Immunization {
+        get throws {
+            return try convert(Immunization.self)
+        }
+    }
+
+    public var procedure: Procedure {
+        get throws {
+            return try convert(Procedure.self)
+        }
+    }
+
     func buildResource() throws -> Resource {
         guard let resource = self.fhirResource else {
             throw HealthKitOnFHIRError.invalidFHIRResource
@@ -28,10 +67,33 @@ extension HKClinicalRecord {
         switch resource.resourceType {
         case .allergyIntolerance:
             return try decode(AllergyIntolerance.self)
+        case .medicationDispense:
+            return try decode(MedicationDispense.self)
+        case .medicationRequest:
+            return try decode(MedicationRequest.self)
+        case .medicationStatement:
+            return try decode(MedicationStatement.self)
+        case .condition:
+            return try decode(Condition.self)
+        case .coverage:
+            return try decode(Coverage.self)
+        case .immunization:
+            return try decode(Immunization.self)
+        case .observation:
+            return try decode(Observation.self)
+        case .procedure:
+            return try decode(Procedure.self)
         default:
             throw HealthKitOnFHIRError.notSupported
         }
 
+    }
+
+    private func convert<T: Resource>(_ type: T.Type = T.self) throws -> T {
+        guard let resource = try buildResource() as? T else {
+            throw HealthKitOnFHIRError.invalidFHIRResource
+        }
+        return resource
     }
 
     private func decode<T: Resource>(
