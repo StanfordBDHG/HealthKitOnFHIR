6c703ee01464b893d1ae93627ef005fde5c949d4
Vishnu Ravi <vishnur@stanford.edu>
2024-12-09 15:50:37 -0800

    Set timezone extension to internal access

--- Sources/HealthKitOnFHIR/HealthKit Extensions/HKSample+TimeZone.swift
+++ Sources/HealthKitOnFHIR/HealthKit Extensions/HKSample+TimeZone.swift
@@ -12,7 +12,7 @@ import HealthKit
 extension HKSample {
     /// Gets the `TimeZone` from the sample's metadata if available
     /// - Returns: A `TimeZone` if the metadata contains a valid HKMetadataKeyTimeZone value, otherwise nil
-    public var timeZone: TimeZone? {
+    internal var timeZone: TimeZone? {
         guard let timeZoneIdentifier = metadata?[HKMetadataKeyTimeZone] as? String else {
             return nil
         }
