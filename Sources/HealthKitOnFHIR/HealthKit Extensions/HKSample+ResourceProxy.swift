6d40b04f19ce02901ccb6e9e79e007490196d6c1
Lukas Kollmer <hey@lukaskollmer.de>
2025-07-09 17:48:31 +0200

    x

--- Sources/HealthKitOnFHIR/HealthKit Extensions/HKSample+ResourceProxy.swift
+++ Sources/HealthKitOnFHIR/HealthKit Extensions/HKSample+ResourceProxy.swift
@@ -84,6 +84,8 @@ extension Sequence where Element: HKSample {
     /// Produces an Array of FHIR `ResourceProxies`.
     ///
     /// - Note: This method provides significant performance improvements as compared to calling ``HealthKit/HKSample/resource(withMapping:issuedDate:extensions:)`` for each element in the collection.
+    ///
+    /// - parameter mapping: A mapping to map `HKSample`s to corresponding FHIR observations allowing the customization of, e.g., codings and units. See ``HKSampleMapping``.
     public func compactMapIntoResourceProxies(
         using mapping: HKSampleMapping = .default,
         issuedDate: FHIRPrimitive<Instant>? = nil,
